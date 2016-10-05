104d6dc224419fda9fe425a8823a7bcfb3193096
zainul <zainulmasadi90@gmail.com>
2016-10-05 15:07:17 +0700

    rename column env in yamls

---
+++ database/migrations/1475654533994_changeColumNameYaml.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class ChangeColumNameYamlSchema extends Schema {
+
+  up () {
+    this.table('yamls', (table) => {
+      table.renameColumn('env', 'data')
+    })
+  }
+
+  down () {
+    this.table('yamls', (table) => {
+      table.renameColumn('data', 'env')
+      // opposite of up goes here
+    })
+  }
+
+}
+
+module.exports = ChangeColumNameYamlSchema
