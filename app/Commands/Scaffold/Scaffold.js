7a1431519124205cad29346e7aa45b1df7d52663
zainul <zainulmasadi90@gmail.com>
2016-10-17 17:51:25 +0700

    adding view generator with split create, edit, field, index, show

--- app/Commands/Scaffold/Scaffold.js
+++ app/Commands/Scaffold/Scaffold.js
@@ -62,7 +62,27 @@ class Scaffold extends BaseGenerator {
     } catch (e) {
       this._error(e.message)
     }
+  }
 
+  * makeView(name) {
+    try {
+      const entity = this._makeEntityName(name, 'view', false)
+      const table  = this._makeEntityName(name, '', false, 'plural')
+      const files = ['index', 'show', 'create', 'edit', 'field'];
+
+      for (var i = 0; i < files.length; i++) {
+        const toPath = path.join(this.helpers.viewsPath(),`${entity.entityName.toLowerCase()}s`, `${files[i]}.njk`)
+        const template = `view_${files[i]}`
+        const templateOptions = {
+          extend: 'master'
+        }
+        yield this._wrapWrite(template, toPath, templateOptions)
+      }
+
+
+    }catch(e) {
+      this._error(e.message)
+    }
   }
 
   get signature () {
@@ -77,9 +97,10 @@ class Scaffold extends BaseGenerator {
     try {
       const name = args.name
       yield this.makeController(name)
-      // yield this.makeModel(name)
-      // yield this.makeRepository(name)
-      this.success("Yeeeaahh finished build , let's code")
+      yield this.makeModel(name)
+      yield this.makeRepository(name)
+      yield this.makeView(name)
+      this.success("Ayee finished build , let's code")
     } catch (e) {
       this._error(e.message)
     }
